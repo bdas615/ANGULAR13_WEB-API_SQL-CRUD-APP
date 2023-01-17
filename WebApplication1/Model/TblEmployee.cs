5f036ebc01089041026d8e2dfd79d4c24311b82a
bdas615 <bdas@brandinstitute.com>
2023-01-17 09:10:08 +0530

    Add project files.

---
+++ WebApplication1/Model/TblEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Model
+{
+    public class TblEmployee
+    {
+        public int Id { get; set; }
+        
+        [StringLength(150)]
+        public string? Name { get; set; }
+
+        [StringLength(150)]
+        public string? LastName { get; set; }
+
+        [StringLength(250)]
+        public string? Email { get; set; }
+        public int Age { get; set; }
+        public DateTime Doj { get; set; }
+        [StringLength(20)]
+        public string? Gender { get; set; }
+        public int IsMarried { get; set; }
+        public int IsActive { get; set; }
+        public int DesignationID { get; set; }
+        [NotMapped]   // not used as column in db
+        public string? Designation { get; set; }
+
+    }
+}
